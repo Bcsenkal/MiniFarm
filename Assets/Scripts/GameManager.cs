6a004d9fd8899cdac2a657ea74fe450d78af10b3
Bilal Can Şenkal <98615099+Bcsenkal@users.noreply.github.com>
2025-03-19 22:41:40 +0300

    last touch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Managers;
+using LitMotion;
+using LitMotion.Extensions;
+using LitMotion.Adapters;
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    void Awake()
+    {
+        //Litmotion causes a frame drop when it is first used, so we are preloading it here to avoid that.
+        MotionDispatcher.EnsureStorageCapacity<Vector3, NoOptions, Vector3MotionAdapter>(10);
+    }
+    
     void Start()
     {
+
         AudioManager.Instance.SetMusicVolume(ES3.Load("musicVolume",1f));
         AudioManager.Instance.SetSFXVolume(ES3.Load("sfxVolume",1f));
         AudioManager.Instance.PlayMusic(true);
